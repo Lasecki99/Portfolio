a7912ad1402914f1bf5f51f32f3e04b01b6251f2
zadaniarekrutacyjne <lasecki00089@gmail.com>
2019-05-15 19:21:39 +0200

    Updates

---
+++ precache-manifest.af0561ec17413a523e982553ad750cc8.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbcc49e423bd85773a224d50799231cc",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "2f098253d71bdf428f0f",
+    "url": "/Portfolio/static/css/main.5f98c55a.chunk.css"
+  },
+  {
+    "revision": "d4a894faf63aae55ce3a",
+    "url": "/Portfolio/static/js/2.1178fec6.chunk.js"
+  },
+  {
+    "revision": "2f098253d71bdf428f0f",
+    "url": "/Portfolio/static/js/main.069273f8.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "20f23dc356f79c3f8d0cd44eb69ada04",
+    "url": "/Portfolio/static/media/banner.20f23dc3.png"
+  },
+  {
+    "revision": "95194eb4c6e33b77a3d14bb13cb3c104",
+    "url": "/Portfolio/static/media/photo1.95194eb4.png"
+  },
+  {
+    "revision": "d6f7fa40086eaa849fc949ba0f65060b",
+    "url": "/Portfolio/static/media/php.d6f7fa40.png"
+  },
+  {
+    "revision": "a0a30ac3324aaeaca7f1ed9aea2343fe",
+    "url": "/Portfolio/static/media/portfolio.a0a30ac3.png"
+  },
+  {
+    "revision": "b2767e217f08c713e8ddbc8194b86f53",
+    "url": "/Portfolio/static/media/rock-paper-scissors.b2767e21.png"
+  },
+  {
+    "revision": "cb60708260aaae906836a9b08adcf4db",
+    "url": "/Portfolio/static/media/slider.cb607082.png"
+  },
+  {
+    "revision": "06fceb407a2549e4967ea62ff5563662",
+    "url": "/Portfolio/static/media/strona.06fceb40.png"
+  },
+  {
+    "revision": "16783fe784ef953ed3781ad3a03dbff2",
+    "url": "/Portfolio/static/media/video.16783fe7.mp4"
+  },
+  {
+    "revision": "58810062a489c3cc8c93f4323c5d8787",
+    "url": "/Portfolio/static/media/weather.58810062.png"
+  }
+]);
